5f40625065138a9fb9fdd6aafe12f6247af8068c
ImranShaikh8806 <imranshaikh880672@gmail.com>
2024-11-08 21:34:52 +0530

    added allen logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Outlet } from 'react-router-dom'
-import Signup from './components/Signup'
-import Signin from './components/Signin'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
